b4f13d0c90575b3c94315626c76ea885597b9f73
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2016-02-13 23:44:32 +0530

    TestNG sample framework for iOS parallel execution

---
+++ src/test/java/com/test/classes/BaseTest.java
@@ -0,0 +1,14 @@
+package com.test.classes;
+
+import java.io.IOException;
+import org.testng.annotations.AfterSuite;
+
+public class BaseTest {
+
+	 CommandPrompt cp = new CommandPrompt();
+	
+	@AfterSuite()
+	public void finalEnd() throws InterruptedException, IOException{
+		cp.runCommand("killall -9 node");
+	}
+}
---
+++ src/test/java/com/test/classes/QTest2.java
@@ -0,0 +1,82 @@
+package com.test.classes;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.AndroidServerFlag;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
+import io.appium.java_client.service.local.flags.IOSServerFlag;
+
+public class QTest2 {
+
+	private IOSDriver<MobileElement> driver;
+	private AppiumDriverLocalService service;
+	
+	
+	AvailabelPorts avlport = new AvailabelPorts();
+	@BeforeClass(alwaysRun = true)
+	public void startApp() throws Exception {
+		System.out.println("Starting Appium Server 2");
+		File classPathRoot = new File(System.getProperty("user.dir"));
+		int port = avlport.getPort();
+		int bootstrapPort=avlport.getPort();
+		AppiumServiceBuilder builder = new AppiumServiceBuilder()
+				.withAppiumJS(new File(
+						"/usr/local/lib/node_modules/appium/bin/appium.js"))
+				.withArgument(GeneralServerFlag.LOG_LEVEL, "info")
+				.withArgument(IOSServerFlag.USE_NATIVE_INSTRUMENTS)
+				.withArgument(AndroidServerFlag.BOOTSTRAP_PORT_NUMBER, Integer.toString(bootstrapPort))
+				.withArgument(GeneralServerFlag.TEMP_DIRECTORY,
+						new File(String.valueOf(classPathRoot)).getAbsolutePath() + "/target/" + "tmp_" + port)
+				.withArgument(GeneralServerFlag.SESSION_OVERRIDE).usingPort(port);
+		service = builder.build();
+		service.start();
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("deviceName", "iPod touch");
+//		capabilities.setCapability("autoLaunch", true);
+		capabilities.setCapability("newCommandTimeout", 90);
+		capabilities.setCapability("udid", "cb5168c0b1397f21189d46532e86147aa122660b");
+		capabilities.setCapability("bundleId", "com.tesco.grocery");
+		capabilities.setCapability("noReset", true);
+		driver = new IOSDriver<MobileElement>(service.getUrl(),capabilities);
+		Thread.sleep(10000L);
+	}
+
+	@AfterClass()
+	public void killServer() throws InterruptedException, IOException {
+		driver.quit();
+	}
+
+	@Test
+	public void testMethodFive_5() throws Exception {
+		System.out.println("ThreadName: " + Thread.currentThread().getName() + Thread.currentThread().getStackTrace()[1].getClassName());
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@name='action_bar_up_navigation'][1]")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@name='Store locator']")).click();
+		Thread.sleep(3000);
+
+	}
+	
+//	@Test
+//
+//	public void testMethodFive_6() throws Exception {
+//		System.out.println("ThreadName: " + Thread.currentThread().getName() + Thread.currentThread().getStackTrace()[1].getClassName());
+//		Thread.sleep(3000);
+//		driver.findElement(By.xpath("//*[@name='action_bar_up_navigation'][1]")).click();
+//		Thread.sleep(3000);
+//		driver.findElement(By.xpath("//*[@name='Store locator']")).click();
+//		Thread.sleep(3000);
+//
+//	}
+}
